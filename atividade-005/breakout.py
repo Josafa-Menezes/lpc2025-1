7a7b93fa6d03827c9f13e8d6085ff3ce483b5d10
Josafa-Menezes <jlmenezes.pro@gmail.com>
2025-09-28 02:17:26 -0400

    Some constants

--- atividade-005/breakout.py
+++ atividade-005/breakout.py
@@ -11,6 +11,15 @@ ORANGE = (255, 100, 0)
 YELLOW = (255, 255, 0)
 COLOR_BLACK = (0, 0, 0)
 
+# Other constants
+BALL_RADIUS = 10
+BALL_SPEED_X = 5
+BALL_SPEED_Y = 5
+PADDLE_WIDTH = 100
+PADDLE_HEIGHT = 20
+PADDLE_SPEED = 7
+FPS = 60
+
 # Screen
 size = (1280, 720)
 screen = pygame.display.set_mode(size)
